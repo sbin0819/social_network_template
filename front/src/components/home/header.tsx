45dd44c47d375d5e7767d0104b421cfaefc48672
sbin0819 <sbinha123@gmail.com>
2021-05-16 17:51:19 +0900

    add: home header

---
+++ front/src/components/home/header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Icon } from '../common';
+import styled from 'styled-components';
+import faker from 'faker';
+import { setDataList } from '../../utils/helperF';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 32px;
+`;
+
+const UserIconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const index = () => {
+  const userList = setDataList(faker.internet, 7);
+  return (
+    <Container>
+      {userList.map((d, i) => (
+        <UserIconContainer key={i}>
+          <Icon size="60" userImg={d.avatar()} />
+          <div>{d.userName().slice(0, 6)}</div>
+        </UserIconContainer>
+      ))}
+    </Container>
+  );
+};
+
+export default index;
---
+++ front/src/components/home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from './header';
+
+const Container = styled.div``;
+
+const index = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default index;
