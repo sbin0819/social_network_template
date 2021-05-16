87d57762e98272c1fd2a7afa71db97b7bfaa14bd
sbin0819 <sbinha123@gmail.com>
2021-05-16 17:51:19 +0900

    add: home header

--- front/src/components/common/Icon.tsx
+++ front/src/components/common/Icon.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { AiOutlineUser } from 'react-icons/ai';
 
 // sm, md, lg
-const Container = styled.div<{size: string}>`
+const Container = styled.div<{ size: string }>`
   width: ${({ size }) => `${size}px`};
   height: ${({ size }) => `${size}px`};
   border-radius: 50px;
@@ -12,14 +12,22 @@ const Container = styled.div<{size: string}>`
   align-items: center;
   border-radius: 50%;
   background: #ddd !important;
+  img {
+    border: 3px solid #35a7c0;
+    border-radius: 50%;
+  }
 `;
 
 interface IProps {
   userImg?: any;
   size?: string;
 }
-const index: React.FC<IProps> = ({ userImg = false, size='40' }) => {
-  return <Container size={size}>{userImg ? 'U' : <AiOutlineUser />}</Container>;
+const index: React.FC<IProps> = ({ userImg = false, size = '40' }) => {
+  return (
+    <Container size={size}>
+      {userImg ? <img src={userImg} /> : <AiOutlineUser />}
+    </Container>
+  );
 };
 
 export default index;
